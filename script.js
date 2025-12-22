ba87a52eb2dce97e6c7d6b5b98d51488a378ebe9
Роман (Roman Nikitin) <roman.nikitin@internera.com>
2025-12-22 17:25:31 +0200

    add if/else

--- script.js
+++ script.js
@@ -4,14 +4,26 @@ const guessedNumber = document.querySelector('.number');
 const scoreElement = document.querySelector('.score');
 
 let score = 20;
+let highScore = 0;
 let guessedNumberValue = Math.trunc(Math.random() * 20 + 1);
-guessedNumber.textContent = guessedNumberValue;
 const btn = document.querySelector('.btn.check');
-
+const inputField = document.querySelector('.guess');
 const message = document.querySelector('.message');
 
+const againBtn = document.querySelector('.again');
+
+againBtn.addEventListener('click', function () {
+  score = 20;
+  scoreElement.textContent = score;
+  document.body.style.backgroundColor = '#222';
+  guessedNumber.style.width = '15rem';
+  guessedNumber.textContent = '?';
+  guessedNumberValue = Math.trunc(Math.random() * 20 + 1);
+  message.textContent = 'Start guessing...';
+  inputField.value = '';
+});
+
 btn.addEventListener('click', function () {
-  const inputField = document.querySelector('.guess');
   const inputFieldValue = Number(inputField.value);
 
   if (!inputField.value) {
@@ -22,18 +34,12 @@ btn.addEventListener('click', function () {
     inputFieldValue > 20
   ) {
     message.textContent = '⛔ Number must be between 1 and 20!';
-  } else if (inputFieldValue > guessedNumberValue) {
+  } else if (inputFieldValue !== guessedNumberValue) {
     if (score > 1) {
-      message.textContent = '📈 the current number is greater than guessed';
-      score--;
-      scoreElement.textContent = score;
-    } else {
-      scoreElement.textContent = 0;
-      message.textContent = '💥 you lost';
-    }
-  } else if (inputFieldValue < guessedNumberValue) {
-    if (score > 1) {
-      message.textContent = '📉 the current number is less than guessed';
+      message.textContent =
+        inputFieldValue > guessedNumberValue
+          ? '📈 the current number is greater than guessed'
+          : '📉 the current number is less than guessed';
       score--;
       scoreElement.textContent = score;
     } else {
@@ -45,5 +51,10 @@ btn.addEventListener('click', function () {
     guessedNumber.textContent = guessedNumberValue;
     document.body.style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
+    guessedNumber.textContent = guessedNumberValue;
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector('.highscore').textContent = highScore;
+    }
   }
 });
